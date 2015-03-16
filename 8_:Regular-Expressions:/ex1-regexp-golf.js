c18a52dc7207a2e16d795b429936b7fdedf98793
sbaldwin24 <sterling.baldwin@gmail.com>
2015-03-16 14:15:29 -0500

    Chapter-8 --- exercise 1 fix, exercise 2 and 3.

--- 8_:Regular-Expressions:/ex1-regexp-golf.js
+++ 8_:Regular-Expressions:/ex1-regexp-golf.js
@@ -31,15 +31,11 @@ function verify(regexp, yes, no) {
   // Ignore unfinished exercises
   if (regexp.source == "...") return;
   yes.forEach(function(s) {
-    if (!regexp.test(s)) {
+    if (!regexp.test(s))
       console.log("Failure to match '" + s + "'");
-      }
-    }
   });
   no.forEach(function(s) {
-    if (regexp.test(s)) {
+    if (regexp.test(s))
       console.log("Unexpected match for '" + s + "'");
-      }
-    }
   });
 }
