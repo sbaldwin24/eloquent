8ebdfafcb4f7f66ce3e9f7a3143b32debf8ea403
sbaldwin24 <sterling.baldwin@gmail.com>
2015-03-16 11:12:15 -0500

    chapter 8 -- the search method

--- 8_:Regular-Expressions:/8-notes.js
+++ 8_:Regular-Expressions:/8-notes.js
@@ -554,5 +554,17 @@ console.log(text.replace(regexp, "_$1_"));
 
 
 
+// The Search Method
+// The indexOf method on strings cannot be called with a regular expression. But there is another method, "search", which does expect
+// a regular expression. Like indexOf, it returns the first index on which the expression was found, or -1 when it wasn't found.
+console.log("      word".search(/\S/));
+// -> 6
+console.log("      ".search(/\S/));
+// -> -1
+// Unfortunately, there is no way to indicate that the match should start at a given offset (like we can with the second argument to
+// indexOf), which can often be useful.
+
+
+// The LastIndex Property
 
 
