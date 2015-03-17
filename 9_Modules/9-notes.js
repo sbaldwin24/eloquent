69a93e1441cbc965fced79f0352126fa1ac0ca92
sbaldwin24 <sterling.baldwin@gmail.com>
2015-03-17 10:50:10 -0500

    Chapter-9 -- notes -- Reuse

--- 9_Modules/9-notes.js
+++ 9_Modules/9-notes.js
@@ -20,3 +20,25 @@
 
 // Though JavaScript provides no actual module construct yet, objects can be used to create publicly accessible subnamespaces, and
 // functions can be used to create an isolated, private namespace inside of a module.
+
+
+// Reusse
+// In a "flat" project, which isn't structured as a set modules, it is not apparent which parts of the code are needed to use a
+// particular function. In the previous chapter, we wrote a function for reading configuration files. If we wanted to use that function
+// in another project, we must go and copy out the parts of the old program that look like they are relevant to the functionality that
+// we need and paste them into our new program. Then, if we find a mistake in that code, we will fix it only in whichever program that
+// we are working with at the time and forget to also fix it in the other program.
+
+// Once you have lots of such shared, duplicated pieces of code, you will find yourself wasting a lot of time and energy on moving them
+// around and keeping them up-to-date.
+
+// Putting pieces of functionality that stand on their own into separate files and modules make them easier to track, update, and share
+// because all the various pieces of code that want to use the module load it from the same actual file.
+
+// This idea get even more powerful when the relations between modules -- which other modules each module depends on -- are explicitly
+// stated. You can then automate the process of intalling and upgrading external modules (libraries).
+
+// Taking this idea even further, imagine an online service that track and distributes hundreds of thousands of such libraries,
+// allowing us to search for the functionality we need and, one we find it, set up our project to automatically download it.
+
+// NPM!
